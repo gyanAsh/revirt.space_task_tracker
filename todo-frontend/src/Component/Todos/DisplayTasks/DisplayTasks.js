a19a1fa8c7edb93604b139ed3e64cea1d24a17a2
gyanAsh <gyanash36@protonmail.com>
2022-07-18 00:49:03 +0530

    Get request from api and rendered task successfully

--- todo-frontend/src/Component/Todos/DisplayTasks/DisplayTasks.js
+++ todo-frontend/src/Component/Todos/DisplayTasks/DisplayTasks.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React,{useContext, useEffect} from 'react'
 import TodoCard from '../TaskLayout/TodoCard'
 import styles from './DisplayTasks.module.css';
+import TasksContext from '../../../Context/Tasks/TasksContext';
+
 const DisplayTasks = () => {
+    const tasksContext = useContext(TasksContext)
+    const { tasks, getTasks } = tasksContext;
+    useEffect(() => {
+        getTasks();
+})
   return (
     <div className={styles.displayTasksContainer}>
         <h3>Added task to-do list</h3>
           <ol className={styles.displayList}>
+              { tasks !== null && tasks.map(task => (
+                  <li key={task.id}>
+                      <TodoCard
+                          title={task.title}
+                          completed={task.completed}
+                      />
+                  </li>
+               ))}
+              
+              {/* <li> <TodoCard/></li>
               <li> <TodoCard/></li>
               <li> <TodoCard/></li>
-              <li> <TodoCard/></li>
-              <li> <TodoCard/></li>
-              <li> <TodoCard/></li>
+              <li> <TodoCard/></li> */}
           </ol>  
     </div>
   )
--- todo-frontend/src/Component/Todos/TaskLayout/TodoCard.js
+++ todo-frontend/src/Component/Todos/TaskLayout/TodoCard.js
@@ -1,14 +1,13 @@
-import React,{useState} from 'react'
+import React from 'react'
 import styles from './TodoCard.module.css';
 
-const TodoCard = () => {
-    const [status, setStatus] = useState("Completed");
+const TodoCard = ({title,completed}) => {
   return (
     <div className={styles.taskContainer}>
-          <h3> Task asdfasdfas dfasasd fasdfasdffff fffffffff ffffdfasdfas dfasddfasdf asdfasdf</h3>
+          <h3> {title}</h3>
           <span></span>
           <div className={styles.inputs}>
-              <button id={styles.status}>Mark as {status}</button>
+              <button id={styles.status}>Mark as { completed ? "C" : "soop"}</button>
               <button id={styles.delete}>Delete</button>
           </div>
     </div>
