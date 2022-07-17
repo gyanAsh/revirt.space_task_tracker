40938201eef18c0f70edbd428c1982edf7963325
gyanAsh <gyanash36@protonmail.com>
2022-07-17 18:42:19 +0530

    InputNewTask Component css added

--- todo-frontend/src/Component/Todos/InputNewTask/InputNewTask.js
+++ todo-frontend/src/Component/Todos/InputNewTask/InputNewTask.js
@@ -1,11 +1,11 @@
 import React,{useState} from 'react'
-
+import styles from './InputNewTask.module.css';
 const InputNewTask = () => {
 
     const [task, setTask] = useState("");
 
   return (
-    <div>
+    <div className={styles.inputTaskContainer}>
         <h3> Add a new task in the list</h3>
           <form>
               <input type="text" name="task" onChange={e=>setTask(e.target.value)} value={task} placeholder="Enter the task here" />
