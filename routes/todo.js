b4c3632a4f029d5ab1bd5921a1f2fe1eae24d3e1
gyanAsh <gyanash36@protonmail.com>
2022-07-21 15:38:58 +0530

    Adding new user's Todo function created

--- routes/todo.js
+++ routes/todo.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const authVerify = require('../middleware/authVerify')
+const authVerify = require('../middleware/authVerify');
+const { body, validationResult } = require('express-validator');
 const Todo = require('../models/Todo');
+const { Http2ServerResponse } = require('http2');
 
 // @route   GET api/v1/todo
 // @desc    Get all of user todos
@@ -14,6 +16,30 @@ router.get('/',authVerify ,async (req, res) => {
         console.error(err.message);
         res.status(500).send("Internal Server Error");
     }
+})
+
+// @route   GET api/v1/todo
+// @desc    POST new todo
+// @access  Private
+router.post('/', authVerify,
+    body('title',"Task cannot be empty").not().isEmpty()
+    , async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { title } = req.body;
+
+        try {
+            const newTodo = new Todo({ user: req.user.id, title });
+            const todo = await newTodo.save();
+            res.json(todo);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    
 })
 
 
