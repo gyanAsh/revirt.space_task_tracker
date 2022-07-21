4d1dea89f7b957a49a52fc902c9d05a902300d91
gyanAsh <gyanash36@protonmail.com>
2022-07-21 18:34:51 +0530

    Updating todo status function implemented

--- routes/todo.js
+++ routes/todo.js
@@ -18,12 +18,12 @@ router.get('/',authVerify ,async (req, res) => {
     }
 })
 
-// @route   GET api/v1/todo
+// @route   POST api/v1/todo
 // @desc    POST new todo
 // @access  Private
 router.post('/', authVerify,
     body('title',"Task cannot be empty").not().isEmpty()
-    , async (req, res) => {
+, async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
@@ -42,6 +42,32 @@ router.post('/', authVerify,
     
 })
 
+// @route   POST api/v1/todo
+// @desc    Update status of existing todo
+// @access  Private
+router.patch('/:id', authVerify, async (req, res) => {
 
+    const { completed } = req.body;
+
+    try {
+        let todo = await Todo.findById(req.params.id);
+
+        if (!todo) {
+            return res.status(404).send("Contact Not Found");
+        }
+        if (req.user.id !== todo.user.toString()) {
+            return res.status(401).send("Unauthorized Request");
+        }
+
+        todo = await Todo.findByIdAndUpdate(req.params.id,
+            {completed},
+            { new: true });
+        return res.json(todo);
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
 
 module.exports = router;
